bc60a08ad69959cefa3b43c2f3b9689a1b5cd1c1
Pablo <pablogarcirodriguez@gmail.com>
2024-08-06 07:44:35 +0200

    Creando componente de productos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,36 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initPoducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 10.99,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 9.99,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 12.99,
+        image: 'https://picsum.photos/640/640?r=15',
+        creationAt: new Date().toISOString(),
+      },
+    ];
+
+    this.products.set(initPoducts);
+  }
+
   fromChild(event: string) {
     console.log('Estamos en el padre');
     console.log(event);
