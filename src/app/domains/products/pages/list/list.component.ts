487773e2eacaeaefbb04b965da017f7cf1fad99d
Pablo <pablogarcirodriguez@gmail.com>
2024-08-08 07:44:43 +0200

    Reactividad con signals en servicios

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { CartService } from '../../../shared/services/cart.service';
 
 @Component({
   selector: 'app-list',
@@ -13,7 +14,7 @@ import { HeaderComponent } from '../../../shared/components/header/header.compon
 })
 export class ListComponent {
   products = signal<Product[]>([]);
-  cart = signal<Product[]>([]);
+  private cartService = inject(CartService);
 
   constructor() {
     const initPoducts: Product[] = [
@@ -79,6 +80,6 @@ export class ListComponent {
   }
 
   addToCart(product: Product) {
-    this.cart.update((prevState) => [...prevState, product]);
+    this.cartService.addToCart(product);
   }
 }
