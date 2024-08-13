db0bbd5577db31210bc80c7b5dc25645eab872f9
Pablo <pablogarcirodriguez@gmail.com>
2024-08-13 10:30:08 +0200

    Detalles del producto e imágenes

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductService } from '@shared/services/product.service';
+import { Input } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent {
+  @Input() id?: string;
+  product = signal<Product | null>(null);
+  private ProductService = inject(ProductService);
+
+  cover = signal('');
+
+  ngOnInit() {
+    if (this.id) {
+      this.ProductService.getOne(this.id).subscribe({
+        next: (product) => {
+          this.product.set(product);
+          if (product.images.length > 0) {
+            this.cover.set(product.images[0]);
+          }
+        },
+      });
+    }
+  }
+
+  changeCover(newImg: string) {
+    this.cover.set(newImg);
+  }
+}
